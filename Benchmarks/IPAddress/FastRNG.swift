ff7e46cc48b46aafd199e4213a865b3930e995ca
MahdiBM <github@mahdibm.com>
2026-07-05 14:39:22 +0330

    Add benchs mimicking real workloads

---
+++ Benchmarks/IPAddress/FastRNG.swift
@@ -0,0 +1,11 @@
+struct FastRNG {
+    var state: UInt64 = 0x9E37_79B9_7F4A_7C15
+
+    @inline(__always)
+    mutating func next() -> UInt64 {
+        self.state ^= self.state &<< 13
+        self.state ^= self.state &>> 7
+        self.state ^= self.state &<< 17
+        return self.state
+    }
+}

a051917212898a65ee3c0b0eacfb8b6982ceda5c
MahdiBM <github@mahdibm.com>
2026-07-06 15:28:10 +0330

    Disable warnings-as-errors in nightlies for now

---
+++ Package@swift-6.4.swift
@@ -0,0 +1,106 @@
+// swift-tools-version: 6.3
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-endpoint",
+    products: [
+        .library(name: "Endpoint", targets: ["Endpoint"]),
+        .library(name: "Domain", targets: ["Domain"]),
+        .library(name: "IPAddress", targets: ["IPAddress"]),
+        .library(name: "DomainIPAddressCompat", targets: ["DomainIPAddressCompat"]),
+    ],
+    traits: [
+        .trait(name: "IDNA_SUPPORT"),
+        .trait(name: "NIO_BYTE_BUFFER_SUPPORT"),
+        /// IDNA to be removed from the default traits in the future
+        .default(enabledTraits: ["IDNA_SUPPORT", "NIO_BYTE_BUFFER_SUPPORT"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-dns/swift-idna.git", from: "1.0.0-beta.25"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.100.0"),
+    ],
+    targets: [
+        .target(
+            name: "Endpoint",
+            dependencies: [
+                "Domain",
+                "IPAddress",
+                "DomainIPAddressCompat",
+                .product(
+                    name: "SwiftIDNA",
+                    package: "swift-idna",
+                    condition: .when(traits: ["IDNA_SUPPORT"])
+                ),
+            ],
+            swiftSettings: settings
+        ),
+        .target(
+            name: "Domain",
+            dependencies: [
+                .product(
+                    name: "NIOCore",
+                    package: "swift-nio",
+                    condition: .when(traits: ["NIO_BYTE_BUFFER_SUPPORT"])
+                ),
+                .product(
+                    name: "SwiftIDNA",
+                    package: "swift-idna",
+                    condition: .when(traits: ["IDNA_SUPPORT"])
+                ),
+            ],
+            swiftSettings: settings
+        ),
+        .target(
+            name: "IPAddress",
+            swiftSettings: settings
+        ),
+        .target(
+            name: "DomainIPAddressCompat",
+            dependencies: [
+                .product(
+                    name: "NIOCore",
+                    package: "swift-nio",
+                    condition: .when(traits: ["NIO_BYTE_BUFFER_SUPPORT"])
+                ),
+                "Domain",
+                "IPAddress",
+            ],
+            swiftSettings: settings
+        ),
+        .testTarget(
+            name: "EndpointTests",
+            dependencies: [
+                .product(
+                    name: "NIOCore",
+                    package: "swift-nio",
+                    condition: .when(traits: ["NIO_BYTE_BUFFER_SUPPORT"])
+                ),
+                "Endpoint",
+            ],
+            swiftSettings: settings
+        ),
+    ]
+)
+
+var settings: [SwiftSetting] {
+    [
+        .swiftLanguageMode(.v6),
+        .strictMemorySafety(),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableExperimentalFeature(
+            "AvailabilityMacro=SwiftStdlib 5.1:macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0"
+        ),
+        .enableExperimentalFeature(
+            "AvailabilityMacro=SwiftStdlib 5.3:macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0"
+        ),
+        .enableExperimentalFeature(
+            "AvailabilityMacro=SwiftStdlib 6.0:macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0"
+        ),
+        .enableExperimentalFeature(
+            "AvailabilityMacro=SwiftStdlib 6.2:macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0"
+        ),
+    ]
+}

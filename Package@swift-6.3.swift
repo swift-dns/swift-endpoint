d74a6d43a7d689082f8b997718c530f2715912a4
MahdiBM <github@mahdibm.com>
2026-08-15 14:56:29 +0330

    Use a C for lookup table in IPv6 serialization

--- Package@swift-6.3.swift
+++ Package@swift-6.3.swift
@@ -53,8 +53,10 @@ let package = Package(
         ),
         .target(
             name: "IPAddress",
+            dependencies: ["CSwiftEndpoint"],
             swiftSettings: settings
         ),
+        .target(name: "CSwiftEndpoint"),
         .target(
             name: "DomainIPAddressCompat",
             dependencies: [

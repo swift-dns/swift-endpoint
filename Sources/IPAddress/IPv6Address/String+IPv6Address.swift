0a307175a5b50344e7baf0d9d5e10f80967aa95b
MahdiBM <github@mahdibm.com>
2026-08-21 17:17:15 +0330

    Try memcpy

--- Sources/IPAddress/IPv6Address/String+IPv6Address.swift
+++ Sources/IPAddress/IPv6Address/String+IPv6Address.swift
@@ -656,7 +656,20 @@ extension IPv6Address: LosslessStringConvertible {
             return false
         }
 
-        address = afterCs | (beforeCs &<< (16 &* (8 &- segmentsCountBeforeCs)))
+        let beforeCsByteOffset = 16 &- (segmentsCountBeforeCs &<< 1)
+        let beforeCsByteCount = segmentsCountBeforeCs &<< 1
+
+        address = afterCs
+        withUnsafeMutableBytes(of: &address) { addressPtr in
+            withUnsafeBytes(of: beforeCs) { beforeCsPtr in
+                unsafe addressPtr.baseAddress.unsafelyUnwrapped
+                    .advanced(by: beforeCsByteOffset)
+                    .copyMemory(
+                        from: beforeCsPtr.baseAddress.unsafelyUnwrapped,
+                        byteCount: beforeCsByteCount
+                    )
+            }
+        }
 
         return true
     }

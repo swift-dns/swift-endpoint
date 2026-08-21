2bf9990b0490c3b13f4684990acc7d32af5a384c
MahdiBM <github@mahdibm.com>
2026-08-21 09:32:15 +0330

    Sync files from swift-dns/shared@37ba773082ede8ab2ace3f4513ff1088599c3bea

--- .github/scripts/wait-for-swift-exit.sh
+++ .github/scripts/wait-for-swift-exit.sh
@@ -7,14 +7,14 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-readonly max_wait_seconds="${MAX_WAIT_SECONDS:-300}"
+readonly max_wait_seconds="${MAX_WAIT_SECONDS:-30}"
 
 waited_seconds=0
 while swift_processes="$(pgrep -l swift)"; do
   if [[ "${waited_seconds}" -ge "${max_wait_seconds}" ]]; then
     fatal "Swift processes were still running after ${max_wait_seconds}s:" "${swift_processes}"
   fi
-  if [[ "$((waited_seconds % 30))" -eq 0 ]]; then
+  if [[ "$((waited_seconds % 10))" -eq 0 ]]; then
     log "Waiting for swift processes to exit:" "${swift_processes}"
   fi
   sleep 1

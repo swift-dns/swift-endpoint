54feda3e6ee2eea9eb51986b4deaede08f53df8d
MahdiBM <github@mahdibm.com>
2026-09-09 12:06:04 +0330

    Fix CI

--- Tests/EndpointTests/ConnectionTargetTests.swift
+++ Tests/EndpointTests/ConnectionTargetTests.swift
@@ -4,67 +4,9 @@ import Testing
 @Suite
 struct ConnectionTargetTests {
     @available(SwiftStdlib 6.0, *)
-    @Test(
-        arguments: [(target: ConnectionTarget?, expected: ConnectionTarget.Target?)]([
-            (
-                target: try? .ipAddress("127.0.0.1", port: 11),
-                expected: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 11)
-            ),
-            (
-                target: .ipAddress(IPv4Address(127, 0, 0, 1), port: 22),
-                expected: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 22)
-            ),
-            (
-                target: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 33),
-                expected: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 33)
-            ),
-            (
-                target: try? .ipAddress("[::1]", port: 44),
-                expected: .ipAddress(.v6(IPv6Address(0x1)), port: 44)
-            ),
-            (
-                target: .ipAddress(IPv6Address(0x1), port: 55),
-                expected: .ipAddress(.v6(IPv6Address(0x1)), port: 55)
-            ),
-            (
-                target: .ipAddress(.v6(IPv6Address(0x1)), port: 66),
-                expected: .ipAddress(.v6(IPv6Address(0x1)), port: 66)
-            ),
-            (
-                target: try? .domainName(DomainName("www.example.com"), port: 77),
-                expected: try? .domainName(DomainName("www.example.com"), port: 77)
-            ),
-            (
-                target: try? .domainName(DomainName("127.0.0.1"), port: 88),
-                expected: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 88)
-            ),
-            (
-                target: try? .domainName(DomainName("::1"), port: 99),
-                expected: nil
-            ),
-            (
-                target: try? .domainName("www.example.com", port: 77),
-                expected: try? .domainName(DomainName("www.example.com"), port: 77)
-            ),
-            (
-                target: try? .domainName("127.0.0.1", port: 88),
-                expected: .ipAddress(.v4(IPv4Address(127, 0, 0, 1)), port: 88)
-            ),
-            (
-                target: try? .domainName("::1", port: 99),
-                expected: nil
-            ),
-            (
-                target: .unixDomainSocketAddress("/tmp/socket"),
-                expected: .unixDomainSocketAddress("/tmp/socket")
-            ),
-        ])
-    )
-    func `static funcs work as expected`(
-        target: ConnectionTarget?,
-        expected: ConnectionTarget.Target?
-    ) throws {
-        #expect(target?.target == expected)
+    @Test(arguments: ConnectionTargetTestCase.targetAndExpectedTarget)
+    func `static funcs work as expected`(testCase: ConnectionTargetTestCase) throws {
+        #expect(testCase.target?.target == testCase.expected)
     }
 
     @available(SwiftStdlib 6.0, *)
